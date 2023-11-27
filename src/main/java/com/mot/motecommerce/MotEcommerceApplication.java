c03c6d56f8a37f9c2d115236706ff1a465f564e7
Volodymyr Shepel <v.shepel158@gmail.com>
2023-11-27 14:32:28 +0100

    Initial commit

---
+++ src/main/java/com/mot/motecommerce/MotEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mot.motecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotEcommerceApplication.class, args);
+	}
+
+}
